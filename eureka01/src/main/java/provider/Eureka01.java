7ec39eef092f4d409bd3ef34a729ed70026f0227
chichengfengye <1639700823@qq.com>
2021-08-13 08:40:22 +0800

    eureka01: eureka01可以访问了

---
+++ eureka01/src/main/java/provider/Eureka01.java
@@ -0,0 +1,13 @@
+package provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka01 {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka01.class);
+    }
+}
